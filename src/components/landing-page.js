a1e6a5d3444bce74cac339ea5c45329c410d4500
Joe-Pena <joepena@protonmail.com>
2018-12-05 08:17:11 -0700

    Landing page css

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 
 import LoginForm from './login-form';
+import './landingpage.css';
 
 export function LandingPage(props) {
-    // If we are logged in redirect straight to the user's dashboard
-    if (props.loggedIn) {
-        return <Redirect to="/dashboard" />;
-    }
+  // If we are logged in redirect straight to the user's dashboard
+  if (props.loggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
 
-    return (
-        <div className="home">
-            <h2>Welcome to Foo App</h2>
-            <LoginForm />
-            <Link to="/register">Register</Link>
-        </div>
-    );
+  return (
+    <div className="landing-page-main-grid">
+      <div className="info-area">
+        <h1 className="info-message">Buvie, find friends to watch your <br />favorite movies with!</h1>
+      </div>
+      <div className="side-login">
+        <h2 className="buvie-landing-logo">Buvie</h2>
+        <LoginForm />
+        <span className="signup-button">Not a member?<Link to="/register">Register</Link></span>
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(LandingPage);
