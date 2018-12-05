57fafd1e51d202234ff10c125a53ea0a7f975b3f
Joe-Pena <joepena@protonmail.com>
2018-12-05 09:18:05 -0700

    add logo

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -4,6 +4,7 @@ import { Link, Redirect } from 'react-router-dom';
 
 import LoginForm from './login-form';
 import './landingpage.css';
+import logo from '../images/buvienamelogo.png';
 
 export function LandingPage(props) {
   // If we are logged in redirect straight to the user's dashboard
@@ -17,7 +18,7 @@ export function LandingPage(props) {
         <h1 className="info-message">Buvie, find friends to watch your <br />favorite movies with!</h1>
       </div>
       <div className="side-login">
-        <h2 className="buvie-landing-logo">Buvie</h2>
+        <img src={logo} className="buvie-landing-logo" />
         <LoginForm />
         <span className="signup-button">Not a member?<Link to="/register">Register</Link></span>
       </div>
