591879392f4d146b2dd6cd62b3d59df9879e149e
Cameron Hatch <cameronlavigne92@gmail.com>
2018-12-14 09:34:36 -0700

    added omdb api key to config

--- src/config.js
+++ src/config.js
@@ -4,3 +4,5 @@ export const BASE_URL =
     process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
 export const GOOGLE_MAP_KEY =
 process.env.REACT_APP_GOOGLE_MAP_KEY || undefined;
+export const OMDB_API_KEY =
+process.env.REACT_APP_OMDB_API_KEY || undefined;
