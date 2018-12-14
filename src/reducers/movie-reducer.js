cda9663df7494a1c5f9c72052e918f5b0fc6e753
Cameron Hatch <cameronlavigne92@gmail.com>
2018-12-14 11:54:52 -0700

    added action types to movie reducer

--- src/reducers/movie-reducer.js
+++ src/reducers/movie-reducer.js
@@ -1,9 +1,16 @@
 import {
-  SET_MOVIE_LIST, RESET_MOVIES
+  SET_MOVIE_LIST,
+  RESET_MOVIES,
+  FETCH_OMDB_INFO_REQUEST,
+  FETCH_OMDB_INFO_SUCCESS,
+  FETCH_OMDB_INFO_ERROR
 } from '../actions/movies-action';
 
 const initialState = {
-  list: []
+  list: [],
+  loading: false,
+  error: null,
+  currentMovie: null
 };
 
 export default function reducer(state = initialState, action) {
@@ -13,6 +20,22 @@ export default function reducer(state = initialState, action) {
     });
   } else if (action.type === RESET_MOVIES) {
     return initialState;
+  } else if (action.type === FETCH_OMDB_INFO_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true,
+      error: null
+    });
+  } else if (action.type === FETCH_OMDB_INFO_SUCCESS) {
+    return Object.assign({}, state, {
+      loading: false,
+      error: null,
+      currentMovie: action.movieInfo
+    });
+  } else if (action.type === FETCH_OMDB_INFO_ERROR) {
+    return Object.assign({}, state, {
+      loading: false,
+      error: action.error
+    });
   }
   return state;
 }
